3fcf761dd5cecc634cb86821f4a6e6d49a8a567c
Lvalon <lvalonlmao@gmail.com>
2023-12-05 22:20:04 -0800

    status effect icon update

---
+++ toolbox.cs
@@ -0,0 +1,46 @@
+﻿using LBoL.Base.Extensions;
+using LBoL.Core;
+using LBoL.Core.Cards;
+using LBoLEntitySideloader.Resource;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace lvalonmima
+{
+    public abstract class toolbox
+    {
+
+        public static GlobalLocalization LocalizationCard(DirectorySource dirsorc)
+        {
+            var loc = new GlobalLocalization(dirsorc);
+            loc.LocalizationFiles.AddLocaleFile(Locale.En, "cardEn.yaml");
+            return loc;
+        }
+        public static GlobalLocalization LocalizationStatus(DirectorySource dirsorc)
+        {
+            var loc = new GlobalLocalization(dirsorc);
+            loc.LocalizationFiles.AddLocaleFile(Locale.En, "SEEn.yaml");
+            return loc;
+        }
+        public static GlobalLocalization LocalizationExhibit(DirectorySource dirsorc)
+        {
+            var loc = new GlobalLocalization(dirsorc);
+            loc.LocalizationFiles.AddLocaleFile(Locale.En, "exEn.yaml");
+            return loc;
+        }
+        public static GlobalLocalization LocalizationUlt(DirectorySource dirsorc)
+        {
+            var loc = new GlobalLocalization(dirsorc);
+            loc.LocalizationFiles.AddLocaleFile(Locale.En, "bombEn.yaml");
+            return loc;
+        }
+        public static GlobalLocalization LocalizationPlayer(DirectorySource dirsorc)
+        {
+            var loc = new GlobalLocalization(dirsorc);
+            loc.LocalizationFiles.AddLocaleFile(Locale.En, "playerEn.yaml");
+            return loc;
+        }
+    }
+}
