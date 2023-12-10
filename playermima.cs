f497ac1b6e43be01973a98d47eb66e7e2a32992d
Lvalon <lvalonlmao@gmail.com>
2023-12-09 19:54:12 -0800

    ultimate spells implemented

---
+++ playermima.cs
@@ -0,0 +1,107 @@
+﻿using LBoLEntitySideloader.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LBoLEntitySideloader;
+using static lvalonmima.BepinexPlugin;
+using LBoLEntitySideloader.Resource;
+using LBoL.ConfigData;
+using LBoLEntitySideloader.Utils;
+using LBoL.Core.Units;
+using LBoLEntitySideloader.Attributes;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace lvalonmima
+{
+    public sealed class playermima : PlayerUnitTemplate
+    {
+        public static DirectorySource dir = new DirectorySource(PluginInfo.GUID, "lvalonmima");
+
+        public static string name = nameof(Mima);
+
+        public override IdContainer GetId() => nameof(Mima);
+
+        public override LocalizationOption LoadLocalization()
+        {
+            return toolbox.locplayer();
+        }
+
+        public override PlayerImages LoadPlayerImages()
+        {
+            var sprites = new PlayerImages();
+
+            var asyncLoading = ResourceLoader.LoadSpriteAsync("playermimaportrait.png", directorySource);
+
+            sprites.SetStartPanelStand(asyncLoading);
+            sprites.SetWinStand(asyncLoading);
+            sprites.SetDeckStand(asyncLoading);
+
+            return sprites;
+        }
+
+        public override PlayerUnitConfig MakeConfig()
+        {
+            var reimuConfig = PlayerUnitConfig.FromId("Reimu").Copy();
+
+            var config = new PlayerUnitConfig(
+            Id: "",
+            ShowOrder: 2147483647,
+            Order: 0,
+            UnlockLevel: 0,
+            ModleName: "",
+            NarrativeColor: "#ffffff",
+            IsSelectable: true,
+            MaxHp: 40,
+            InitialMana: new LBoL.Base.ManaGroup() { Blue = 2, Black = 2 },
+            InitialMoney: 120,
+            InitialPower: 0,
+            //temp
+            UltimateSkillA: "ulta",
+            UltimateSkillB: "ultb",
+            ExhibitA: "mimaa",
+            ExhibitB: "mimab",
+            DeckA: new List<string> { "Shoot", "Shoot", "Shoot", "Shoot", "Shoot", "Shoot", "Shoot", "Shoot", "Shoot", "Shoot" },
+            DeckB: new List<string> { "Shoot", "Shoot", "Shoot", "Shoot", "Shoot", "Shoot", "Shoot", "Shoot", "Shoot", "Shoot" },
+            DifficultyA: 3,
+            DifficultyB: 3
+            );
+            return config;
+        }
+
+        [EntityLogic(typeof(playermima))]
+        public sealed class Mima : PlayerUnit { }
+    }
+
+    public sealed class mimamodel : UnitModelTemplate
+    {
+
+
+        public override IdContainer GetId() => new playermima().UniqueId;
+
+        public override LocalizationOption LoadLocalization()
+        {
+            return toolbox.locmodel();
+        }
+
+        public override ModelOption LoadModelOptions()
+        {
+            return new ModelOption(ResourceLoader.LoadSpriteAsync("playermimasprite.png", directorySource, ppu: 336));
+        }
+
+
+        public override UniTask<Sprite> LoadSpellSprite() => ResourceLoader.LoadSpriteAsync("playermimabomb.png", playermima.dir, ppu: 336);
+
+
+        public override UnitModelConfig MakeConfig()
+        {
+
+            var config = UnitModelConfig.FromName("Reimu").Copy();
+            config.Flip = true;
+            config.Type = 0;
+            config.Offset = new Vector2(0, -0.1f);
+            return config;
+
+        }
+    }
+}
---
+++ ult.cs
@@ -0,0 +1,135 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLEntitySideloader.Entities;
+using LBoLEntitySideloader.Resource;
+using LBoL.Core;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Battle;
+using LBoL.Core.Cards;
+using LBoL.Core.Units;
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using static lvalonmima.BepinexPlugin;
+using static lvalonmima.SE.evilspiritdef;
+using Unity.IO.LowLevel.Unsafe;
+using System.Collections;
+
+namespace lvalonmima
+{
+    public sealed class ultadef : UltimateSkillTemplate
+    {
+        public override IdContainer GetId() => nameof(ulta);
+
+        public override LocalizationOption LoadLocalization()
+        {
+            return toolbox.locult();
+        }
+
+        public override Sprite LoadSprite()
+        {
+            return ResourceLoader.LoadSprite("ulta.png", embeddedSource);
+        }
+
+        public override UltimateSkillConfig MakeConfig()
+        {
+            var config = new UltimateSkillConfig(
+                Id: "",
+                Order: 10,
+                PowerCost: 80,
+                PowerPerLevel: 80,
+                MaxPowerLevel: 2,
+                RepeatableType: UsRepeatableType.OncePerBattle,
+                Damage: 15,
+                Value1: 999,
+                Value2: 3,
+                Keywords: Keyword.None,
+                RelativeEffects: new List<string>() { },
+                RelativeCards: new List<string>() { }
+                );
+
+            return config;
+        }
+
+        [EntityLogic(typeof(ultadef))]
+        public sealed class ulta : UltimateSkill
+        {
+            public ulta()
+            {
+                base.TargetType = TargetType.RandomEnemy;
+                base.GunName = "Butterfly1";
+            }
+            protected override IEnumerable<BattleAction> Actions(UnitSelector selector)
+            {
+                yield return new DamageAction(base.Owner, base.Owner, DamageInfo.HpLose(Value1, true), "Instant", GunType.Single);
+                int atktimeleft = Value2;
+                while ( atktimeleft > 0 && !base.Battle.BattleShouldEnd)
+                {
+                    yield return new DamageAction(base.Owner, selector.GetEnemy(base.Battle), this.Damage, base.GunName, GunType.Single);
+                    atktimeleft--;
+                }
+            }
+        }
+    }
+
+    public sealed class ultbdef : UltimateSkillTemplate
+    {
+        public override IdContainer GetId() => nameof(ultb);
+
+        public override LocalizationOption LoadLocalization()
+        {
+            return toolbox.locult();
+        }
+
+        public override Sprite LoadSprite()
+        {
+            return ResourceLoader.LoadSprite("ultb.png", embeddedSource);
+        }
+
+        public override UltimateSkillConfig MakeConfig()
+        {
+            var config = new UltimateSkillConfig(
+                Id: "",
+                Order: 10,
+                PowerCost: 120,
+                PowerPerLevel: 120,
+                MaxPowerLevel: 2,
+                RepeatableType: UsRepeatableType.FreeToUse,
+                Damage: 0,
+                Value1: 666,
+                Value2: 0,
+                Keywords: Keyword.None,
+                RelativeEffects: new List<string>() { },
+                RelativeCards: new List<string>() { }
+                );
+
+            return config;
+        }
+
+        [EntityLogic(typeof(ultbdef))]
+        public sealed class ultb : UltimateSkill
+        {
+            public ultb()
+            {
+                base.TargetType = TargetType.RandomEnemy;
+            }
+            protected override IEnumerable<BattleAction> Actions(UnitSelector selector)
+            {
+                yield return new DamageAction(base.Owner, base.Owner, DamageInfo.HpLose(Value1, true), "Instant", GunType.Single);
+                if (base.Battle.BattleShouldEnd)
+                {
+                    yield break;
+                }
+                yield return new DamageAction(base.Owner, base.Owner, DamageInfo.HpLose(Value1, true), "Instant", GunType.Single);
+                if (base.Battle.BattleShouldEnd)
+                {
+                    yield break;
+                }
+            }
+        }
+    }
+}
